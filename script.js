8f2e2f2e2881fcc32c6195eeb54abbda6f7f3ecd
Paula Carrano <paula.agustina.carrano@gmail.com>
2020-11-12 17:52:55 -0300

    punto1

---
+++ script.js
@@ -0,0 +1,116 @@
+// PUNTO 1 
+
+const precioMaquina = (componentes) => {
+    let precios = local.precios;
+    let total = 0;
+
+    for (componente of componentes) {
+        let encontrar = precios.find(component => component.componente == componente);
+        total += encontrar.precio
+    }
+
+    return total;
+}
+
+///console.log(precioMaquina(["Monitor GPRS 3000", "Motherboard ASUS 1500"])); // 320 ($200 del monitor + $120 del motherboard)
+
+const cantidadVentasComponente = (componente) => {
+    let ventas = local.ventas;
+    let cantidad = [];
+
+    for (venta of ventas) {
+        for (component of venta.componentes) {
+            if (component.includes(componente)) {
+                cantidad.push(component)
+            }
+        }
+    }
+    return cantidad.length;
+}
+//console.log(cantidadVentasComponente("Monitor ASC 543")); // 2
+
+const vendedoraDelMes = (mes, anio) => {
+    let ventas = local.ventas;
+    let vendedoras = [];
+    let montoMax = 0;
+
+
+    for (venta of ventas) {
+        if ((venta.fecha.getMonth() + 1) == mes && (venta.fecha.getFullYear()) == anio) {
+            let totalVentas = precioMaquina(venta.componentes);
+            let vendedoraName = venta.nombreVendedora;
+            let vendedora = {}
+
+            vendedora.total = totalVentas;
+            vendedora.vendedoraNombre = vendedoraName;
+
+
+            if (!vendedoras.some(v => v.vendedoraNombre == venta.nombreVendedora)) {
+                vendedoras.push(vendedora);
+            } else {
+                let resultado = vendedoras.find(element => element.vendedoraNombre == vendedora.vendedoraNombre);
+
+                resultado.total += vendedora.total;
+
+                if (resultado.total > montoMax) {
+                    montoMax = resultado.total;
+                    nameVendedora = vendedora.vendedoraNombre;
+                    return nameVendedora
+                }
+            }
+        };
+    }
+}
+
+//console.log(vendedoraDelMes(1, 2019)); // "Ada" (vendio por $670, una máquina de $320 y otra de $350)
+
+const ventasMes = (mes, anio) => {
+    let ventas = local.ventas;
+    let total = 0;
+
+    for (venta of ventas) {
+        if ((venta.fecha.getMonth() + 1) == mes && (venta.fecha.getFullYear()) == anio) {
+            let totalVentas = precioMaquina(venta.componentes);
+            total += totalVentas;
+        }
+    }
+    return total;
+}
+
+//console.log(ventasMes(1, 2019)); // 1250
+
+const ventasVendedora = (nombre) => {
+    let ventas = local.ventas;
+    let total = 0;
+
+    for (venta of ventas) {
+        if (nombre == venta.nombreVendedora) {
+            let totalVentas = precioMaquina(venta.componentes);
+            total += totalVentas
+        }
+    }
+    return total
+}
+//console.log(ventasVendedora("Grace")); // 900
+
+const componenteMasVendido = () => {
+
+}
+
+//console.log(componenteMasVendido()); // Monitor GPRS 3000
+
+const huboVentas = (mes, anio) => {
+    let ventas = local.ventas;
+    let boolean = false;
+
+    for (venta of ventas) {
+        if ((venta.fecha.getMonth() + 1) != mes && (venta.fecha.getFullYear()) == anio && venta.componentes.length != 0) {
+            continue
+        } else {
+            boolean = true;
+        }
+    }
+    return boolean
+}
+
+//console.log(huboVentas(3, 2019)); // false
